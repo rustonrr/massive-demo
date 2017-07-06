38298850b8fdd497f6d6c534bb1f64eda5cc8e06
Ruston Reformado <rustonreformado@gmail.com>
2017-07-06 13:31:19 -0600

    morning sql practice

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var massive = require('massive');
+var _ = require("lodash");
+
+var db = massive.connectSync({
+  connectionString: 'postgres://obtfrkyhepvhlk:287ad58ef357fbd951068f368c6ad7c8c27db6da634658749fee519504fa7342@ec2-54-235-72-121.compute-1.amazonaws.com:5432/d9n6vd7vc3cu9q?ssl=true'
+})
 
 var app = express();
 app.use(bodyParser.json());
@@ -7,15 +13,38 @@ app.use(bodyParser.json());
 var port = 3000;
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  // db.getAllInjuries(function(err, injuries) {
+  //   res.send(injuries) 
+  // });
+  var cake = _.template(`
+    <h1>hello <%=name %></h1>
+  `)
+  var html = cake({
+    name: "Ruston"
+  })
+  res.send(cake({name: "Ruston"}))
 });
 
 app.get('/incidents', function(req, res) {
-  res.send([]);
+  var state = req.query.state;
+
+  if(state) {
+    db.getIncidentsByState([state], function(err, incidents){
+      res.send(incidents);
+    });
+  }
+  else {
+      db.getAllIncidents(function(err, incidents){
+    res.send(incidents);
+      });
+  }
 });
 
 app.post('/incidents', function(req, res) {
-  res.send({id: 123});
+  var incident = req.body;
+  db.createIncident([incident.state, incident.injuryId, incident.causeId], function(err, result){
+    res.send(result[0]);
+  });
 });
 
 app.listen(port, function() {
